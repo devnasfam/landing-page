bd5465877a0a8778ec8ca33b9995e6d3ba1dceea
Dev Nasfam <90545069+devnasfam@users.noreply.github.com>
2023-10-14 20:16:46 +0100

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -1,44 +1,53 @@
-import React, {useState} from 'react';
-function Nav(){
-  
+import React, { useState } from 'react';
+
+function Nav() {
   const [status, setStatus] = useState(false);
 
   const toggleMenu = () => {
     setStatus(!status);
   };
-  
+
   const closeMenu = () => {
     setStatus(false);
   };
-  
 
   const menuWidth = status ? '70%' : '0';
-  const boxShadoww = status ?  '0 0 0 999px rgba(0,0,0,.6)':'0 0 0 rgba(0,0,0,0)';
-      return(
-          <>
-          <nav>
-          <div className="icon"></div>
-          <div className="bars_cont">
-            <i 
+  const boxShadoww = status ? '0 0 0 999px rgba(0,0,0,.6)' : '0 0 0 rgba(0,0,0,0)';
+
+  return (
+    <>
+      <nav>
+        <div className="icon"></div>
+        <div className="bars_cont">
+          <i
             className="fa fa-bars"
             onClick={toggleMenu}
-            >
-            </i>
-          </div>
-            <ul id="menu">
-                <li> Home</li>
-                <li> Contact</li>
-                <li> About</li>
-            </ul>
-          </nav>
-          <ul id="menu_mobile" style={{width:menuWidth,boxShadow:boxShadoww}}>
-              <li onClick={closeMenu}><i className="fa fa-arrow-left"></i></li>
-                <li><i className="fa fa-home"></i> Home</li>
-                <li><i className="fa fa-user-circle"></i> Contact</li>
-                <li><i className="fa fa-info-circle"></i> About</li>
-            </ul>
-          </>
-      );
-  }
+            aria-expanded={status ? 'true' : 'false'}
+            aria-label="Toggle Menu"
+          ></i>
+        </div>
+        <ul id="menu">
+          <li>Home</li>
+          <li>Contact</li>
+          <li>About</li>
+        </ul>
+      </nav>
+      <ul id="menu_mobile" style={{ width: menuWidth, boxShadow: boxShadoww }}>
+        <li onClick={closeMenu}>
+          <i className="fa fa-arrow-left" aria-hidden="true"></i> Back
+        </li>
+        <li>
+          <i className="fa fa-home" aria-hidden="true"></i> Home
+        </li>
+        <li>
+          <i className="fa fa-user-circle" aria-hidden="true"></i> Contact
+        </li>
+        <li>
+          <i className="fa fa-info-circle" aria-hidden="true"></i> About
+        </li>
+      </ul>
+    </>
+  );
+}
 
 export default Nav;
