444538223bb534c22a877f967b0f4dd5ba83ed5c
Dev Nasfam <90545069+devnasfam@users.noreply.github.com>
2023-10-14 19:13:24 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{Fragment} from 'react';
+import Nav from './Nav';
+import Header from './Header';
+import Sections from './Sections';
+import Footer from './Footer';
+import './style.css';
+
+let App = () =>{
+    <>
+     <Nav/>
+     <Header/>
+     <Sections/>
+     <Footer/>
+    </>
+}
+export default App;
