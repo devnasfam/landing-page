afb882fff4cc083c9e85fc6f920076bfe3837bcc
Dev Nasfam <90545069+devnasfam@users.noreply.github.com>
2023-10-14 19:38:01 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React,{ StrcitMode } from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
 
-const root = createRoot(document.querySelector("#root");
+const root = createRoot(document.querySelector("#root"));
 root.render(
   <StrcitMode>
    <App/>
