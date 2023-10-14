685dfc440f88edc1872a2eafab9da01d7f780bb7
Dev Nasfam <90545069+devnasfam@users.noreply.github.com>
2023-10-14 19:10:30 +0100

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,57 @@
+import React from 'react';
+function Footer(){
+      return(
+          <>
+          <footer>
+          <div className="desk">
+          <div className="falldivs">
+          
+            <div className="first_divs">
+            <div className="social_div">
+             <div className="social_head">
+              <h4>Follow Us</h4>
+              </div>
+              <ul>
+                  <li><i className="fab fa-instagram"></i> Instagram</li>
+                  <li><i className="fab fa-facebook"></i> Facebook</li>
+                  <li><i className="fab fa-twitter"></i> Twitter</li>
+              </ul>
+             </div>
+            </div>
+            
+            <div className="first_divs">
+            <div className="social_div">
+             <div className="social_head">
+              <h4>Projects</h4>
+              </div>
+              <ul>
+                  <li><i className="fab fa-github"></i> Github</li>
+                  <li><i className="fab fa-linkedin"></i> LinkedIn</li>
+                  <li><i className="fab fa-youtube"></i> YouTube</li>
+              </ul>
+             </div>
+            </div>
+            
+          </div>
+          
+           <div className="first_divs">
+             <div className="social_head">
+              <h4>Send Us An Email</h4>
+             </div>
+             <div class="inputs_cont">
+                <input 
+                type="email"
+                placeholder="Email Address"
+                />
+                <button>Send</button>
+             </div>
+           </div>
+           </div>
+          <div className="copyright">
+            copyright @devnasfam 2023
+          </div>
+          </footer>
+          </>
+      );
+  }
+export default Footer;
